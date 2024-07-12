75c058e35cff61967b8e56c1ad3469f74d0ba9de
eishaa-e <eishae937@gmail.com>
2024-07-13 02:17:29 +0500

    backend configurations

--- backend/controllers/chatbot.controller.js
+++ backend/controllers/chatbot.controller.js
@@ -1,20 +1,21 @@
 const ChatbotInteraction = require("../models/chatbotInteraction.model");
 // Placeholder function for interaction with ML model
-const chatbotService = require("../services/chatbotService");
+// const chatbotService = require("../services/chatbotService");
 
 exports.interact = async (req, res) => {
-  try {
-    const { user_id, message } = req.body;
-    const response = await chatbotService.getResponse(message);
-    const newInteraction = new ChatbotInteraction({
-      user_id,
-      message,
-      response,
-      timestamp: new Date(),
-    });
-    await newInteraction.save();
-    res.json(newInteraction);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+    try {
+        const {user_id, message} = req.body;
+        const response = "";
+        // const response = await chatbotService.getResponse(message);
+        const newInteraction = new ChatbotInteraction({
+            user_id,
+            message,
+            response,
+            timestamp: new Date(),
+        });
+        await newInteraction.save();
+        res.json(newInteraction);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,29 @@
-const express = require("express");
-const app = express();
-const port = 5000;
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth.routes');
+const appointmentRoutes = require('./routes/appointment.routes');
+const chatbotRoutes = require('./routes/chatbot.routes');
+const medicalImageRoutes = require('./routes/medicalImage.routes');
 const connectToMongo = require("./db");
-var cors = require("cors");
+const cors = require("cors");
+const port = 5000;
+
+const app = express();
+app.use(express.json());
 
 connectToMongo();
 
 app.use(cors());
-app.use(express.json());
 
-// API Routes
-app.use("/api/auth", require("./routes/auth"));
-//app.use("/api/food", require("./routes/food"));
+app.use('/api/auth', authRoutes);
+app.use('/api/appointments', appointmentRoutes);
+app.use('/api/chatbot', chatbotRoutes);
+app.use('/api/medical-image', medicalImageRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello Peter!");
+    res.send("Hello DOC Link server!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
 });
