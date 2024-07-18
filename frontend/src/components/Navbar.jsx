4fb831c65ade79d12e1cf600e44274e69ed7eb54
eishaa-e <eishae937@gmail.com>
2024-07-19 00:46:29 +0500

    login, signup button on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -74,150 +74,138 @@ const Navbar = () => {
         getUser();
     }, [authToken, currentUserRole]);
 
-    return (
-        <div>
-            <nav className="bg-fuchsia-100 border-gray-200 dark:bg-gray-900 w-full">
-                <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
-                    <div className="flex items-center">
-                        <img className="w-12 mr-3" src={logo} alt="Logo"/>
-                        <span className=" text-light-orchid text-3xl font-bold whitespace-nowrap">
+    return (<div>
+        <nav className="bg-fuchsia-100 border-gray-200 dark:bg-gray-900 w-full">
+            <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+                <div className="flex items-center">
+                    <img className="w-12 mr-3" src={logo} alt="Logo"/>
+                    <span className=" text-light-orchid text-3xl font-bold whitespace-nowrap">
                             DOC LINK
                         </span>
-                    </div>
-                    <div className="flex items-center space-x-3">
-                        <ul className="flex font-medium space-x-8 rtl:space-x-reverse">
-                            <li>
-                                <a
-                                    href="#"
-                                    className="py-2 px-3 text-blue-700 rounded md:p-0 md:dark:text-blue-500"
-                                    aria-current="page"
-                                >
-                                    Home
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    About Us
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Services
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Contact
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                </div>
+                <div className="flex items-center space-x-3">
+                    <ul className="flex font-medium space-x-8 rtl:space-x-reverse">
+                        <li>
+                            <a
+                                href="#"
+                                className="py-2 px-3 text-blue-700 rounded md:p-0 md:dark:text-blue-500"
+                                aria-current="page"
+                            >
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                About Us
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Services
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
 
-                    <div className="flex items-center flex-col md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                        {!isUserLoggedIn ?
-                            (
-                                <div>
-                                    <Link to="/login"
-                                          className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                                        Login
-                                    </Link>
-                                    <Link to="/signup"
-                                          className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                                        Signup
-                                    </Link>
-                                </div>
-                            ) : (
-                                <div>
-                                    <button type="button"
-                                            className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                                            id="user-menu-button" aria-expanded="false"
-                                            data-dropdown-toggle="user-dropdown"
-                                            data-dropdown-placement="bottom"
-                                            onClick={toggleDropdown}
-                                    >
-                                        <span className="sr-only">Open user menu</span>
-                                        <img className="w-8 h-8 rounded-full"
-                                             src="https://plus.unsplash.com/premium_photo-1681996484614-6afde0d53071?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                             alt="user photo"/>
-                                    </button>
+                <div className="flex items-center flex-col md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    {!isUserLoggedIn ? (<div>
+                        <Link to="/login"
+                              className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                            Login
+                        </Link>
+                        <Link to="/signup"
+                              className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                            Signup
+                        </Link>
+                    </div>) : (<div>
+                        <button type="button"
+                                className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                                id="user-menu-button" aria-expanded="false"
+                                data-dropdown-toggle="user-dropdown"
+                                data-dropdown-placement="bottom"
+                                onClick={toggleDropdown}
+                        >
+                            <span className="sr-only">Open user menu</span>
+                            <img className="w-8 h-8 rounded-full"
+                                 src="https://plus.unsplash.com/premium_photo-1681996484614-6afde0d53071?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                 alt="user photo"/>
+                        </button>
 
-                                    {isDropdownOpen && (
-                                        <div
-                                            className="z-100 fixed my-10 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                                            id="user-dropdown">
-                                            {userInfo &&
-                                                <div className="px-4 py-3">
+                        {isDropdownOpen && (<div
+                            className="z-100 fixed my-10 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                            id="user-dropdown">
+                            {userInfo && <div className="px-4 py-3">
                                     <span
                                         className="block text-sm text-gray-900 dark:text-white">{userInfo.name}</span>
-                                                    <span
-                                                        className="block text-sm  text-gray-500 truncate dark:text-gray-400">{userInfo.user_id.email}</span>
-                                                </div>
-                                            }
-                                            <ul className="py-2" aria-labelledby="user-menu-button">
-                                                <li>
-                                                    <Link to=""
-                                                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</Link>
-                                                </li>
-                                                <li>
-                                                    <Link to=""
-                                                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</Link>
-                                                </li>
-                                                <li>
-                                                    <Link to=""
-                                                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</Link>
-                                                </li>
-                                                <li>
-                                                    <button onClick={handleLogout}
-                                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                                                        out
-                                                    </button>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    )}
-                                </div>
-                            )
-                        }
+                                <span
+                                    className="block text-sm  text-gray-500 truncate dark:text-gray-400">{userInfo.user_id.email}</span>
+                            </div>}
+                            <ul className="py-2" aria-labelledby="user-menu-button">
+                                <li>
+                                    <Link to=""
+                                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</Link>
+                                </li>
+                                <li>
+                                    <Link to=""
+                                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</Link>
+                                </li>
+                                <li>
+                                    <Link to=""
+                                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</Link>
+                                </li>
+                                <li>
+                                    <button onClick={handleLogout}
+                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                                        out
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>)}
+                    </div>)}
 
-                    </div>
-                    <button
-                        data-collapse-toggle="navbar-user"
-                        type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="navbar-user"
-                        aria-expanded={isMenuOpen}
-                        onClick={toggleMenu}
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <svg
-                            className="w-5 h-5"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 17 14"
-                        >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M1 1h15M1 7h15M1 13h15"
-                            />
-                        </svg>
-                    </button>
                 </div>
-            </nav>
-        </div>
-    );
+                <button
+                    data-collapse-toggle="navbar-user"
+                    type="button"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-user"
+                    aria-expanded={isMenuOpen}
+                    onClick={toggleMenu}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 17 14"
+                    >
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M1 1h15M1 7h15M1 13h15"
+                        />
+                    </svg>
+                </button>
+            </div>
+        </nav>
+    </div>);
 };
 
 export default Navbar;
