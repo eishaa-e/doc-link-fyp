5d62c924aed8e6f5ae586e0a6915052f08508624
eishaa-e <eishae937@gmail.com>
2024-08-28 02:12:29 +0500

    Get patient by id API created

--- backend/routes/patient.routes.js
+++ backend/routes/patient.routes.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const patientController = require("../controllers/patient.controller");
-const {authenticateToken} = require('../middleware/auth.middleware');
+const { authenticateToken } = require("../middleware/auth.middleware");
 
 // Patient routes
-router.get("/get-profile", authenticateToken, patientController.getPatientProfile);
+router.get(
+  "/get-profile",
+  authenticateToken,
+  patientController.getPatientProfile,
+);
 router.get("/", authenticateToken, patientController.getAllPatients);
-router.put("/update-profile", authenticateToken, patientController.updatePatientProfile);
-
+router.put(
+  "/update-profile",
+  authenticateToken,
+  patientController.updatePatientProfile,
+);
+router.get(
+  "/get-profile/:id",
+  authenticateToken,
+  patientController.getPatientProfileById,
+);
 
 module.exports = router;
