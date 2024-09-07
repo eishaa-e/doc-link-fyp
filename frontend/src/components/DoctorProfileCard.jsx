100bbffbad5c606f38ae05d379622c96c119b758
eishaa-e <eishae937@gmail.com>
2024-09-07 20:40:26 +0500

    Some updates

--- frontend/src/components/DoctorProfileCard.jsx
+++ frontend/src/components/DoctorProfileCard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-const DoctorProfileCard = ({ doctor }) => {
+const DoctorProfileCard = ({doctor}) => {
     if (!doctor) return null;
 
-    const { _id, name, specialization, education } = doctor; // Destructure the doctor object
+    const {_id, name, specialization, education} = doctor; // Destructure the doctor object
 
     return (
-        <div className="w-full flex justify-center items-center" >
+        <div className="w-full flex justify-center items-center">
             <div
-                className="w-2/3 px-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                className="w-2/3 px-10 bg-fuchsia-100 border border-gray-200 rounded-lg shadow">
                 <div className="flex p-5 justify-between items-center">
                     <div className="flex items-center gap-10">
                         <div className="flex flex-col rounded-full items-center md:flex-row ">
@@ -43,13 +43,13 @@ const DoctorProfileCard = ({ doctor }) => {
                             </Link>
                             <Link
                                 to={`/doctor/${_id}`}
-                                className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-fuchsia-100 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                             >
                                 View Profile
                             </Link>
                             <Link
                                 to="/"
-                                className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-fuchsia-100 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                             >
                                 Message
                             </Link>
@@ -57,7 +57,7 @@ const DoctorProfileCard = ({ doctor }) => {
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 };
 
--- frontend/src/screens/FindDoctor.jsx
+++ frontend/src/screens/FindDoctor.jsx
@@ -1,56 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import DoctorProfileCard from "../components/DoctorProfileCard";
 import axios from "axios";
 
 const FindDoctor = () => {
-  const [doctors, setDoctors] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [doctors, setDoctors] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  const getAllDoctors = async () => {
-    await axios
-      .get("http://localhost:5000/api/doctors/", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-        },
-      })
-      .then((response) => {
-        setLoading(false);
-        setDoctors(response.data);
-        console.log("Doctors: ", response.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+    const getAllDoctors = async () => {
+        await axios
+            .get("http://localhost:5000/api/doctors/", {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+                },
+            })
+            .then((response) => {
+                setLoading(false);
+                setDoctors(response.data);
+                console.log("Doctors: ", response.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    };
 
-  useEffect(() => {
-    getAllDoctors();
-  }, []);
+    useEffect(() => {
+        getAllDoctors();
+    }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div>
-      <div className="w-full h-screen flex flex-col items-center text-black bg-fuchsia-100">
-        <h2 className="text-6xl font-bold my-5"> Choose the Doctor </h2>
-        <div className="w-full flex flex-col justify-center items-center gap-5">
-          {doctors &&
-            doctors.map((doctor, index) => {
-              return (
-                <div
-                  key={index}
-                  className="w-2/3 flex justify-center items-center text-center"
-                >
-                  <DoctorProfileCard doctor={doctor} />
+    return (
+        <div>
+            <div className="w-full h-screen flex flex-col items-center text-black bg-white">
+                <h2 className="text-6xl font-bold my-5"> Choose the Doctor </h2>
+                <div className="w-full flex flex-col justify-center items-center gap-5">
+                    {doctors &&
+                        doctors.map((doctor, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className="w-2/3 flex justify-center items-center text-center"
+                                >
+                                    <DoctorProfileCard doctor={doctor}/>
+                                </div>
+                            );
+                        })}
                 </div>
-              );
-            })}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default FindDoctor;
