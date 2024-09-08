80427988a6b83ec3a0c8f97c6f7a2e08bd9854c3
eishaa-e <eishae937@gmail.com>
2024-09-09 02:07:18 +0500

    Navbar and Footer UI Updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -43,28 +43,57 @@ export const Footer = () => {
     };
 
     return (
-        <div className="w-full bg-fuchsia-100 flex justify-center align-middle items-center p-10">
-            <div className="flex items-center justify-center gap-16">
-                <div className="w-1/3 flex justify-center flex-col">
-                    <Link to="/">
-                        <div className="flex items-center">
-                            <img className="w-12 mr-3" src={logo} alt="Logo"/>
-                            <span className=" text-light-orchid text-3xl font-bold whitespace-nowrap">
-                             DOC LINK
-                            </span>
-                        </div>
-                    </Link>
-                    <div className="w-full flex flex-col justify-center gap-5 mt-4">
-                        <p className="text-sm font-normal">
-                            DOC LINK provides progressive, and affordable healthcare, accessible
-                            online for everyone.
-                        </p>
-                        <p className="text-xs font-normal">
-                            ©DOC LINK LTD 2024. All rights reserved
-                        </p>
-                    </div>
+        <div className="w-full bg-light-orchid flex flex-col justify-center align-middle items-center p-10">
+            <Link to="/">
+                <div className="w-2/3 flex justify-center items-center">
+                    <img className="w-20 mr-3" src={logo} alt="Logo"/>
+                    <span className=" text-white text-3xl font-bold whitespace-nowrap">
+                        DOC LINK
+                    </span>
                 </div>
-                <div className="w-1/3 flex flex-col justify-start items-center">
+            </Link>
+
+            <hr className="w-2/3 h-[2px] bg-white border-0 rounded my-10"/>
+
+            <div className="w-2/3 flex items-start justify-center gap-16">
+                <div className="w-1/3 flex flex-col items-start justify-start">
+                    <h2 className="text-white text-xl font-bold whitespace-nowrap">Reach us</h2>
+
+                    <ul className="flex flex-col justify-center text-white gap-5 mt-4">
+                        <li className="flex gap-5 text-sm font-normal">
+                            <svg className="w-6 h-6 text-white" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                 viewBox="0 0 24 24">
+                                <path
+                                    d="M7.978 4a2.553 2.553 0 0 0-1.926.877C4.233 6.7 3.699 8.751 4.153 10.814c.44 1.995 1.778 3.893 3.456 5.572 1.68 1.679 3.577 3.018 5.57 3.459 2.062.456 4.115-.073 5.94-1.885a2.556 2.556 0 0 0 .001-3.861l-1.21-1.21a2.689 2.689 0 0 0-3.802 0l-.617.618a.806.806 0 0 1-1.14 0l-1.854-1.855a.807.807 0 0 1 0-1.14l.618-.62a2.692 2.692 0 0 0 0-3.803l-1.21-1.211A2.555 2.555 0 0 0 7.978 4Z"/>
+                            </svg>
+                            +92 123 4567890
+                        </li>
+                        <li className="flex gap-5 text-sm font-normal">
+                            <svg className="w-6 h-6 text-white" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                 viewBox="0 0 24 24">
+                                <path
+                                    d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z"/>
+                                <path
+                                    d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z"/>
+                            </svg>
+                            doclink@gmail.com
+                        </li>
+                        <li className="flex gap-5 text-sm font-normal">
+                            <svg className="w-6 h-6 text-white" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                 viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                      d="M11.906 1.994a8.002 8.002 0 0 1 8.09 8.421 7.996 7.996 0 0 1-1.297 3.957.996.996 0 0 1-.133.204l-.108.129c-.178.243-.37.477-.573.699l-5.112 6.224a1 1 0 0 1-1.545 0L5.982 15.26l-.002-.002a18.146 18.146 0 0 1-.309-.38l-.133-.163a.999.999 0 0 1-.13-.202 7.995 7.995 0 0 1 6.498-12.518ZM15 9.997a3 3 0 1 1-5.999 0 3 3 0 0 1 5.999 0Z"
+                                      clip-rule="evenodd"/>
+                            </svg>
+                            132 Dartmouth Street Boston, Massachusetts 02156 United States
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="w-1/3 flex flex-col justify-start items-center text-white">
                     <ul>
                         <li className="text-lg font-normal my-2">
                             <Link to="">
@@ -89,15 +118,13 @@ export const Footer = () => {
                     </ul>
                 </div>
                 <div className="w-1/3 flex justify-center items-center">
-                    <form className="w-11/12 bg-white flex flex-col justify-center items-center p-5 rounded-xl">
+                    <form className="w-2/3 bg-white flex flex-col justify-center items-center p-5 rounded-xl">
                         <h2 className="text-xl font-bold"></h2>
-                        <div className="w2/3">
-                            <textarea name="comment" id="" rows="4" className="bg-fuchsia-100 rounded-xl border-0"
-                                      placeholder="Give us a review"
-                                      value={feedback.comment}
-                                      onChange={handleOnChange}>
-                            </textarea>
-                        </div>
+                        <textarea name="comment" id="" rows="3" className="bg-fuchsia-100 w-full rounded-xl border-0"
+                                  placeholder="Give us a review"
+                                  value={feedback.comment}
+                                  onChange={handleOnChange}>
+                        </textarea>
                         <div className="mt-2">
                             <StarRating
                                 rating={feedback.rating}
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -97,12 +97,12 @@ const Navbar = () => {
     }, [authToken, currentUserRole]);
 
     return (
-        <nav className="bg-fuchsia-100 border-gray-200 dark:bg-gray-900 w-full">
+        <nav className="bg-light-orchid border-gray-200 w-full">
             <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
                 <Link to="/">
                     <div className="flex items-center">
                         <img className="w-12 mr-3" src={logo} alt="Logo"/>
-                        <span className=" text-light-orchid text-3xl font-bold whitespace-nowrap">
+                        <span className=" text-white text-3xl font-bold whitespace-nowrap">
                               DOC LINK
                             </span>
                     </div>
@@ -113,7 +113,7 @@ const Navbar = () => {
                         <li>
                             <Link
                                 to="/"
-                                className="py-2 px-3 text-blue-700 rounded md:p-0 md:dark:text-blue-500"
+                                className="py-2 px-3 text-white rounded md:p-0"
                                 aria-current="page"
                             >
                                 Home
@@ -122,7 +122,7 @@ const Navbar = () => {
                         <li>
                             <Link
                                 to="/"
-                                className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                className="py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                             >
                                 About Us
                             </Link>
@@ -130,7 +130,7 @@ const Navbar = () => {
                         <li>
                             <Link
                                 to="/"
-                                className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                className="py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                             >
                                 Services
                             </Link>
@@ -139,7 +139,7 @@ const Navbar = () => {
                             <li>
                                 <Link
                                     to="/find-doctor"
-                                    className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    className="py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     Find Doctor
                                 </Link>
@@ -148,7 +148,7 @@ const Navbar = () => {
                         <li>
                             <Link
                                 to="/"
-                                className="py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                className="py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                             >
                                 Contact us
                             </Link>
@@ -174,15 +174,24 @@ const Navbar = () => {
                             <div>
                                 <button
                                     type="button"
-                                    className="p-2 flex bg-light-orchid text-white rounded-lg "
+                                    className="text-white text-sm text-center font-bold rounded-lg flex justify-center items-center gap-1 px-2 py-2 bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200"
                                     id="user-menu-button"
                                     aria-expanded="false"
                                     data-dropdown-toggle="user-dropdown"
                                     data-dropdown-placement="bottom"
                                     onClick={toggleDropdown}
                                 >
-                                    {userInfo?.name}
+                                    {userInfo?.name.toUpperCase()}
+
                                     <svg className="w-6 h-6 text-white" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                         viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2"
+                                              d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                    </svg>
+
+                                    <svg className="w-5 h-5 text-white" aria-hidden="true"
                                          xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                                          viewBox="0 0 24 24">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -193,10 +202,10 @@ const Navbar = () => {
 
                                 {isDropdownOpen && (
                                     <div
-                                        className="z-100 p-2 fixed my-10 text-base list-none bg-fuchsia-100 rounded-lg shadow"
+                                        className="z-100 p-2 w-40 fixed my-2 text-base list-none bg-fuchsia-100 rounded-lg divide-y divide-light-orchid shadow"
                                         id="user-dropdown"
                                     >
-                                        <ul aria-labelledby="user-menu-button">
+                                        <ul aria-labelledby="user-menu-button" className="">
                                             <li>
                                                 <Link
                                                     to={
@@ -204,20 +213,18 @@ const Navbar = () => {
                                                             ? `/doctor/${userInfo._id}`
                                                             : `/patient/${userInfo._id}`
                                                     }
-                                                    className="block px-4 py-2 rounded-md text-sm text-gray-700 hover:bg-white"
+                                                    className="block px-4 py-2 rounded-md text-sm text-gray-700 hover:bg-light-orchid hover:text-white"
                                                 >
                                                     Profile
                                                 </Link>
                                             </li>
-                                            <li>
-                                                <button
-                                                    onClick={handleLogout}
-                                                    className="block px-4 py-2 rounded-m text-sm text-gray-700 hover:bg-white"
-                                                >
-                                                    Log out
-                                                </button>
-                                            </li>
                                         </ul>
+                                        <div className="py-1">
+                                            <Link onClick={handleLogout}
+                                                  className="block px-4 py-2 rounded-md text-sm text-gray-700 hover:bg-light-orchid hover:text-white">
+                                                Sign out
+                                            </Link>
+                                        </div>
                                     </div>
                                 )}
                             </div>
