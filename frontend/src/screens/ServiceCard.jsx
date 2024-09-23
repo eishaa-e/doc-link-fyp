690f42a1d8acd321d57deac8138598f0c82e99fb
eishaa-e <eishae937@gmail.com>
2024-09-24 01:00:44 +0500

    appointment-list

--- frontend/src/screens/ServiceCard.jsx
+++ frontend/src/screens/ServiceCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const ServiceCard = ({icon, title, description}) => {
-    return (
-        <div className="bg-fuchsia-100 p-6 rounded-lg shadow-md">
-            <img src={icon} alt={title} className="mb-4 mx-auto"/>
-            <h3 className="text-lg font-semibold mb-2">{title}</h3>
-            <p className="text-gray-600">{description}</p>
-        </div>
-    );
+const ServiceCard = ({ img, title, description }) => {
+  return (
+    <div className="bg-fuchsia-100 p-6 rounded-lg shadow-md">
+      <img src={img} alt={title} className="mb-4 mx-auto" />
+      <h3 className="text-lg font-semibold mb-2">{title}</h3>
+      <p className="text-gray-600">{description}</p>
+    </div>
+  );
 };
 
 export default ServiceCard;
