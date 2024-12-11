72e1835cff9e23980666383a68355163dd683f9b
eishaa-e <eishae937@gmail.com>
2024-12-12 00:16:37 +0500

    sorting of edit schedule

--- frontend/src/screens/DoctorScheduleForm.jsx
+++ frontend/src/screens/DoctorScheduleForm.jsx
@@ -12,7 +12,7 @@ const DoctorScheduleForm = () => {
     "WEDNESDAY",
     "THURSDAY",
     "FRIDAY",
-    "SATURDAY"
+    "SATURDAY",
   ];
   const [selectedDay, setSelectedDay] = useState(daysOfWeek[0]);
   const [selectedSlots, setSelectedSlots] = useState({});
@@ -69,7 +69,7 @@ const DoctorScheduleForm = () => {
     "05:00 AM",
     "05:30 AM",
     "06:00 AM",
-    "06:30 AM"
+    "06:30 AM",
   ];
 
   // Convert 12-hour time format to 24-hour format
@@ -96,7 +96,7 @@ const DoctorScheduleForm = () => {
       filledSlots[slot.dayOfWeek].push({
         dayOfWeek: slot.dayOfWeek,
         startTime: slot.startTime,
-        endTime: slot.endTime
+        endTime: slot.endTime,
       });
       filledSchedule.push(slot);
     });
@@ -127,25 +127,43 @@ const DoctorScheduleForm = () => {
     const newSlot = {
       dayOfWeek: selectedDay,
       startTime: startTime24,
-      endTime
+      endTime,
     };
 
     setSchedule((prevSchedule) => [...prevSchedule, newSlot]);
 
-    setSelectedSlots((prev) => ({
-      ...prev,
-      [selectedDay]: [...(prev[selectedDay] || []), newSlot]
-    }));
+    setSelectedSlots((prev) => {
+      const updatedSlots = [...(prev[selectedDay] || []), newSlot].sort(
+        (a, b) =>
+          convertTo24Hour(a.startTime).localeCompare(
+            convertTo24Hour(b.startTime),
+          ),
+      );
+      return {
+        ...prev,
+        [selectedDay]: updatedSlots,
+      };
+    });
   };
 
   // Remove selected slot from the schedule
   const removeSlot = (day, slot) => {
-    setSelectedSlots((prev) => ({
-      ...prev,
-      [day]: prev[day].filter((s) => s.startTime !== slot.startTime)
-    }));
+    setSelectedSlots((prev) => {
+      const updatedSlots = prev[day]
+        .filter((s) => s.startTime !== slot.startTime)
+        .sort((a, b) =>
+          convertTo24Hour(a.startTime).localeCompare(
+            convertTo24Hour(b.startTime),
+          ),
+        );
+      return {
+        ...prev,
+        [day]: updatedSlots,
+      };
+    });
+
     setSchedule((prevSchedule) =>
-      prevSchedule.filter((s) => s.startTime !== slot.startTime)
+      prevSchedule.filter((s) => s.startTime !== slot.startTime),
     );
   };
 
@@ -181,14 +199,12 @@ const DoctorScheduleForm = () => {
 
   return (
     <div className="w-full bg-gray-100 py-10">
-      <div
-        className="w-full max-w-7xl flex flex-col justify-center items-center mx-auto">
+      <div className="w-full max-w-7xl flex flex-col justify-center items-center mx-auto">
         <h2 className="text-2xl text-center font-bold my-5">
           Manage Availability Schedule
         </h2>
 
-        <div
-          className="w-full flex justify-start items-start gap-6 p-10 bg-white rounded-xl shadow-xl shadow-teal-100">
+        <div className="w-full flex justify-start items-start gap-6 p-10 bg-white rounded-xl shadow-xl shadow-teal-100">
           {/* Days of the week selection */}
           <div className="flex justify-start flex-col gap-5 my-5 px-5 border-r-2 border-gray-300">
             <h2 className="text-2xl font-bold text-center">
@@ -219,7 +235,9 @@ const DoctorScheduleForm = () => {
                     key={index}
                     onClick={() => handleSlotSelection(slot)}
                     className={`py-3 px-4 rounded-xl border shadow-lg my-4 ${selectedSlots[selectedDay]?.some((s) => s.startTime === convertTo24Hour(slot)) ? "bg-teal-500 text-white shadow-teal-100" : "bg-teal-100"} hover:bg-teal-800 hover:text-white`}
-                    disabled={selectedSlots[selectedDay]?.some((s) => s.startTime === convertTo24Hour(slot))}
+                    disabled={selectedSlots[selectedDay]?.some(
+                      (s) => s.startTime === convertTo24Hour(slot),
+                    )}
                   >
                     {slot}
                   </button>
@@ -228,7 +246,6 @@ const DoctorScheduleForm = () => {
             )}
           </div>
         </div>
-
 
         <div className="w-full bg-white my-10 p-10 shadow-xl shadow-teal-100 rounded-xl">
           <h2 className="text-2xl font-bold text-center flex flex-col justify-center items-center">
@@ -248,16 +265,21 @@ const DoctorScheduleForm = () => {
                     <p>
                       {slot.startTime} - {slot.endTime}
                     </p>
-                    <button
-                      onClick={() => removeSlot(day, slot)}
-                    >
-                      <svg className="w-5 h-5 text-red-600" aria-hidden="true"
-                           xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                           fill="currentColor"
-                           viewBox="0 0 24 24">
-                        <path fillRule="evenodd"
-                              d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.757-1a1 1 0 1 0 0 2h8.486a1 1 0 1 0 0-2H7.757Z"
-                              clipRule="evenodd" />
+                    <button onClick={() => removeSlot(day, slot)}>
+                      <svg
+                        className="w-5 h-5 text-red-600"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm5.757-1a1 1 0 1 0 0 2h8.486a1 1 0 1 0 0-2H7.757Z"
+                          clipRule="evenodd"
+                        />
                       </svg>
                     </button>
                   </div>
@@ -265,7 +287,6 @@ const DoctorScheduleForm = () => {
               </div>
             </div>
           ))}
-
         </div>
         <button
           onClick={handleSubmit}
